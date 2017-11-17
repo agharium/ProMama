ba9bea26eb3289fedc6ddddd86f7bfec0ec1b29c
José Paulo Oliveira Filho <oliveira.filho237@gmail.com>
2017-11-17 14:34:54 -0200

    splashscreen at added on android project + refactor of android images

--- ProMama/ProMama.Android/MainActivity.cs
+++ ProMama/ProMama.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace ProMama.Droid
 {
-    [Activity(Label = "Pró-Mamá", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Pró-Mamá", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ ProMama/ProMama.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace ProMama.Droid
+{
+    [Activity(Label = "Pró-Mamá", Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+    }
+}
