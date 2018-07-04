d8f644b4217da1d2c03ed6eb16529591ac36ea1a
José Paulo Oliveira Filho <oliveira.filho237@gmail.com>
2018-07-04 12:46:25 -0300

    Fotos de Postos de Saúde adicionadas e configuradas no layout

--- ProMama/ProMama.Android/Resources/Resource.Designer.cs
+++ ProMama/ProMama.Android/Resources/Resource.Designer.cs
@@ -2285,26 +2285,26 @@ namespace ProMama.Droid
 			// aapt resource value: 0x7f020056
 			public const int avd_hide_password = 2130837590;
 			
-			// aapt resource value: 0x7f02012c
-			public const int avd_hide_password_1 = 2130837804;
+			// aapt resource value: 0x7f02013b
+			public const int avd_hide_password_1 = 2130837819;
 			
-			// aapt resource value: 0x7f02012d
-			public const int avd_hide_password_2 = 2130837805;
+			// aapt resource value: 0x7f02013c
+			public const int avd_hide_password_2 = 2130837820;
 			
-			// aapt resource value: 0x7f02012e
-			public const int avd_hide_password_3 = 2130837806;
+			// aapt resource value: 0x7f02013d
+			public const int avd_hide_password_3 = 2130837821;
 			
 			// aapt resource value: 0x7f020057
 			public const int avd_show_password = 2130837591;
 			
-			// aapt resource value: 0x7f02012f
-			public const int avd_show_password_1 = 2130837807;
+			// aapt resource value: 0x7f02013e
+			public const int avd_show_password_1 = 2130837822;
 			
-			// aapt resource value: 0x7f020130
-			public const int avd_show_password_2 = 2130837808;
+			// aapt resource value: 0x7f02013f
+			public const int avd_show_password_2 = 2130837823;
 			
-			// aapt resource value: 0x7f020131
-			public const int avd_show_password_3 = 2130837809;
+			// aapt resource value: 0x7f020140
+			public const int avd_show_password_3 = 2130837824;
 			
 			// aapt resource value: 0x7f020058
 			public const int baby = 2130837592;
@@ -2912,11 +2912,11 @@ namespace ProMama.Droid
 			// aapt resource value: 0x7f020121
 			public const int notification_icon_background = 2130837793;
 			
-			// aapt resource value: 0x7f02012a
-			public const int notification_template_icon_bg = 2130837802;
+			// aapt resource value: 0x7f020139
+			public const int notification_template_icon_bg = 2130837817;
 			
-			// aapt resource value: 0x7f02012b
-			public const int notification_template_icon_low_bg = 2130837803;
+			// aapt resource value: 0x7f02013a
+			public const int notification_template_icon_low_bg = 2130837818;
 			
 			// aapt resource value: 0x7f020122
 			public const int notification_tile_bg = 2130837794;
@@ -2928,19 +2928,64 @@ namespace ProMama.Droid
 			public const int plugin_lc_smallicon = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int roundedbg = 2130837797;
+			public const int posto_aguapes = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int roundedbgdark = 2130837798;
+			public const int posto_albatroz = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int splash = 2130837799;
+			public const int posto_arroio = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int splash_screen = 2130837800;
+			public const int posto_atlantida = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int youtube_round = 2130837801;
+			public const int posto_baixada = 2130837801;
+			
+			// aapt resource value: 0x7f02012a
+			public const int posto_borussia = 2130837802;
+			
+			// aapt resource value: 0x7f02012b
+			public const int posto_caravagio = 2130837803;
+			
+			// aapt resource value: 0x7f02012c
+			public const int posto_central = 2130837804;
+			
+			// aapt resource value: 0x7f02012d
+			public const int posto_gloria = 2130837805;
+			
+			// aapt resource value: 0x7f02012e
+			public const int posto_laranjeiras = 2130837806;
+			
+			// aapt resource value: 0x7f02012f
+			public const int posto_palmital = 2130837807;
+			
+			// aapt resource value: 0x7f020130
+			public const int posto_passinhos = 2130837808;
+			
+			// aapt resource value: 0x7f020131
+			public const int posto_primavera = 2130837809;
+			
+			// aapt resource value: 0x7f020132
+			public const int posto_santaluzia = 2130837810;
+			
+			// aapt resource value: 0x7f020133
+			public const int posto_serramar = 2130837811;
+			
+			// aapt resource value: 0x7f020134
+			public const int roundedbg = 2130837812;
+			
+			// aapt resource value: 0x7f020135
+			public const int roundedbgdark = 2130837813;
+			
+			// aapt resource value: 0x7f020136
+			public const int splash = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int splash_screen = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int youtube_round = 2130837816;
 			
 			static Drawable()
 			{
