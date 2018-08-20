3542a823ed25779a8db11631955a2030e8bf7f14
José Paulo Oliveira Filho <oliveira.filho237@gmail.com>
2018-08-20 10:04:40 -0300

    ios notifications fix

--- ProMama/ProMama.iOS/AppDelegate.cs
+++ ProMama/ProMama.iOS/AppDelegate.cs
@@ -33,15 +33,18 @@ namespace ProMama.iOS
             {
                 // Ask the user for permission to get notifications on iOS 10.0+
                 UNUserNotificationCenter.Current.RequestAuthorization(
-                        UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
-                        (approved, error) => { });
+                    UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound,
+                    (approved, error) => { });
+
+                // Watch for notifications while app is active
+                UNUserNotificationCenter.Current.Delegate = new UserNotificationCenterDelegate();
             }
             else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
             {
                 // Ask the user for permission to get notifications on iOS 8.0+
                 var settings = UIUserNotificationSettings.GetSettingsForTypes(
-                        UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
-                        new NSSet());
+                    UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound, 
+                    new NSSet());
 
                 UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
             }
